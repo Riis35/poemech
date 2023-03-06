cf5e3ee5a4690dd2a559af500dd175125de38c77
Riis35 <farukozkan56@hotmail.com>
2023-03-06 19:06:47 +0300

    ONURALP KUDURDU BEN DE KOMİTLEDİM

--- src/pages/Login2.js
+++ src/pages/Login2.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import './Login2.css';
+//import './Login2.css';
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
