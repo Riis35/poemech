9fe07d44cbf02662f192422510dbc5ca4e5f4884
hackerFaruk <onuralparslan97@gmail.com>
2023-03-06 19:27:22 +0300

    pis faruk logini

---
+++ src/loginPage/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LoginCss from './Login.module.css'
+
+export default function Login() {
+
+
+
+  return (
+    <div className={LoginCss.containerbox}>
+
+      <input type="text" placeholder=' usher name' />
+      <input type="text" placeholder=' şöfra' />
+
+    </div>
+  )
+}
