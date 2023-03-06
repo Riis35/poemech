3e3df53667e12f53d6f7f92740455ac91ded93b7
hackerFaruk <onuralparslan97@gmail.com>
2023-03-07 01:35:13 +0300

    basic login page that doesnt break production

--- src/loginPage/Login.jsx
+++ src/loginPage/Login.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import LoginCss from './Login.module.css'
+import React, {useState} from "react";
+import LoginCss from "./Login.module.css";
 
 export default function Login() {
 
+ const [userInput, setInput] = useState({username: '',password:''})
 
+
+const handleTyping = ({target}) =>{
+  if(target.id === "username"){setInput(()=> target.value)}
+  if (target.id === "password"){setInput(()=> target.value)}
+  console.log(target.id + " is " + target.value)
+}
 
   return (
-    <div className={LoginCss.containerbox}>
+   <div className={LoginCss.page}>
 
-      <input type="text" placeholder=' usher name' />
-      <input type="text" placeholder=' şöfra' />
+      <div className={LoginCss.containerbox}>
 
+        <label className={LoginCss.inputLabel} > Kullanıcı Adı </label>
+        <input type="text" id="username" value={userInput.username} onChange={handleTyping} />
+
+        <label  className={LoginCss.inputLabel} > Şifre </label>
+        <input type="text" id="password"  value={userInput.password}  onChange={handleTyping} />
+
+        <button> Giriş Yap</button>
+      </div>
     </div>
-  )
+    
+  );
 }
