1c0a0f0371875d75ca5737c4bdeb59127d6e26e5
hackerFaruk <onuralparslan97@gmail.com>
2023-03-08 23:46:07 +0300

    prettier code

--- src/pages/CabinInfo.jsx
+++ src/pages/CabinInfo.jsx
@@ -1,50 +1,45 @@
 import { useEffect, useState } from "react";
-import axios from 'axios';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useParams}
-    from 'react-router-dom';
-  import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useParams,
+} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../navbar/navbar";
 import DeviceList from "../components/DeviceList";
-import CabinCss from './CabinInfo.module.css'
+import CabinCss from "./CabinInfo.module.css";
 
+function CabinInfo() {
+  const navigate = useNavigate();
 
+  const { id } = useParams();
+  const data = () => {
+    axios
+      .post(`${process.env.REACT_APP_URL}/api/CabinInfo`, { id: id })
+      .then((response) => {
+        console.log(response);
+      });
+  };
 
-function CabinInfo () {
-    const navigate = useNavigate();
-    
-    const {id} = useParams();
-    const data = () =>{
-
-
-        axios.post(`${process.env.REACT_APP_URL}/api/CabinInfo`,
-        {id: id,
-        }).then((response) => {
-          console.log(response);
-
-        })
-      };
-
-  const logout = () =>{
+  const logout = () => {
     localStorage.removeItem("token");
     navigate("/login");
   };
 
-  useEffect(() => {
-  data();
+  useEffect(() => {
+    data();
+  }, []);
 
-  }, []);
-
-    return (
-      
-      <div clas={CabinCss.pager} >
-              <Navbar class={CabinCss.navpos}/>
-              <DeviceList/>
-       
-       </div>
-      
-    );
-  
-};
+  return (
+    <div clas={CabinCss.pager}>
+      <Navbar class={CabinCss.navpos} />
+      <DeviceList />
+    </div>
+  );
+}
 
 export default CabinInfo;
