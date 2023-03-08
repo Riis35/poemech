cfa45eb0d73cb3a2266b1b1211f875c3cd261ccc
hackerFaruk <onuralparslan97@gmail.com>
2023-03-08 13:24:56 +0300

    collapse works

--- src/components/Device.jsx
+++ src/components/Device.jsx
@@ -1,20 +1,27 @@
 import DeviceCss from './Device.module.css';
 
-import React from 'react';
+import React, {useState} from 'react';
 import TankContainer from './TankContainer';
 
+
 export default function Device(props){
+    const [collapse, setCollapse] = useState('yes');
 
 
 
-    const alertme =()=>{alert("bas bas bas")}
+
+    const handleCollapse =()=>{
+        setCollapse((prev)=>  { const faruk = prev === "yes" ? "no":"yes"; return faruk} );
+        console.log(collapse)
+    }
    
    
         return(
             
             <div className={DeviceCss.container}>
-                <button className={DeviceCss.name} onClick={alertme}> Cihaz Adı : {props.name}</button>
-                <div className={DeviceCss.tankContainer}> <TankContainer isCollapsed="yes"   />  </div>
+                <button className={DeviceCss.name} onClick={handleCollapse}> Cihaz Adı : {props.name}</button>
+                <div className={DeviceCss.tankContainer}> <TankContainer key={collapse} isCollapsed= {`${collapse}`}  />  </div>
+                
             </div>
         
         )
