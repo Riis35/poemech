4ba709b70a4195ad7bfb658393b7e92a6c5021d5
hackerFaruk <onuralparslan97@gmail.com>
2023-03-26 15:29:26 +0300

    olanlar oldu

--- src/components/Device.jsx
+++ src/components/Device.jsx
@@ -33,7 +33,7 @@ export default function Device(props) {
 
   // eğer yüzde 5 altında ise alarm verme yeri bura isUnderFivePercent true ise alrm veriyo false ise takmıyo
   
-  let  isUnderFivePercent = true ;
+  let  isUnderFivePercent = false ;
   const bgStyle = {};
    if(isUnderFivePercent) { bgStyle =  { backgroundColor : "red"};}
    else { bgStyle = { backgroundColor: colorPalette[parseInt(props.color)]}}
