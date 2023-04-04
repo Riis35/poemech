f568170cae232f9a537bde614bd3216fe8c33ad4
Riis35 <farukozkan56@hotmail.com>
2023-04-04 15:50:04 +0300

    noluyo aq

--- src/Operations/Operations.jsx
+++ src/Operations/Operations.jsx
@@ -9,13 +9,15 @@ import axios from 'axios';
 import differenceBy from 'lodash/differenceBy';
 import Donut from "./DonutChart.tsx"
 import Chart from './DateChart.tsx';
-
+import styled from 'styled-components';
 
 export default function Company(props) {  
     const [datas, setdata] = useState([]);
     const [data, setRows] = useState([]);
     const {id} = useParams();
     const role = localStorage.getItem("top")
+    const [filterText, setFilterText] = React.useState('');
+    const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false);
 
     useEffect(() => {
         getData();
@@ -46,7 +48,7 @@ export default function Company(props) {
                 const starter =  el.Cab_id < 10 ? "ABY0000" : el.Cab_id < 100 ? "ABY000" : el.Cab_id < 1000 ? "ABY00" : el.Cab_id < 10000 ? "ABY0" : "ABY";
                 return {
                     Com_name: starter + el.Cab_id,
-                    Cab_name: el.Cab_name,
+                    Cab_name: el.Com_name,
                     Date: el.Date,
                     Card_id: el.Card_id,
                     Operation: el.Operation,
@@ -74,22 +76,24 @@ export default function Company(props) {
     }
 
 
-    const columns = [{
-        name: 'Kabin Numarası',
-        selector: row => row.Com_name,
-        sortable: true,
-        style: {
-            
-        },
-    },
+    const columns = [
     {
-      name: 'Kabin Adı',
+      name: 'Şirket Adı',
       selector: row => row.Cab_name,
       sortable: true,
       style: {
         			
         		},
     },
+    {
+      name: 'Makine No',
+      selector: row => row.Com_name,
+      left: true,
+      sortable: true,
+      style: {
+          
+      },
+  },
     {
         name: 'Tarih',
         selector: row => row.Date,
@@ -117,6 +121,19 @@ export default function Company(props) {
       }
     
     ]
+
+
+
+
+
+
+  const filteredItems = data.filter(
+    item => item.Cab_name && item.Cab_name.toLowerCase().includes(filterText.toLowerCase()),
+  );
+      
+    
+
+
 
     createTheme(
         'solarized',
@@ -147,6 +164,51 @@ export default function Company(props) {
 
     //<div className={maincss["line-1"]}></div>
 
+    function convertArrayOfObjectsToCSV(array) {
+      let result;
+    
+      const columnDelimiter = ',';
+      const lineDelimiter = '\n';
+      const keys = Object.keys(data[0]);
+    
+      result = '';
+     result += keys.join(columnDelimiter);
+      result += lineDelimiter;
+    
+      array.forEach(item => {
+        let ctr = 0;
+        keys.forEach(key => {
+          if (ctr > 0) result += columnDelimiter;
+    
+          result += item[key];
+          
+          ctr++;
+        });
+        result += lineDelimiter;
+      });
+    
+      return result;
+    }
+    
+    // Blatant "inspiration" from https://codepen.io/Jacqueline34/pen/pyVoWr
+    function downloadCSV(array) {
+      const link = document.createElement('a');
+      let csv = convertArrayOfObjectsToCSV(array);
+      if (csv == null) return;
+    
+      const filename = 'export.csv';
+    
+      if (!csv.match(/^data:text\/csv/i)) {
+        csv = `data:text/csv;charset=utf-8,${csv}`;
+      }
+    
+      link.setAttribute('href', encodeURI(csv));
+      link.setAttribute('download', filename);
+      link.click();
+    }
+
+    const Export = ({ onExport }) => <button className={maincss.newButton} onClick={e => onExport(e.target.value)}>Excel</button>;
+
     return (
         <div className= {maincss.container}>
             <div className={maincss.donut}>
@@ -155,13 +217,16 @@ export default function Company(props) {
             <div>
             {role === "0" ? null : <Chart id = {id}></Chart> }
             </div>
-            
+            <div className={maincss.filter}>
+            <Export onExport={() => downloadCSV(data)} />
+            <input className={maincss.pdiv} type="text" id="search" placeholder='Şirket Adı' onChange={(e) => setFilterText(e.target.value)}/>
+            </div>
             <div className={maincss.partialcontainer}>
             <p >İşlemler</p>
             <div className={maincss["line-1"]}></div>
             <div className={maincss.containerinside}><DataTable
         columns={columns}
-        data={data}
+        data={filteredItems}
         highlightOnHover= {true}
         striped = {true}
         defaultSortFieldId={3}
@@ -177,3 +242,5 @@ export default function Company(props) {
         
     );
 }
+
+
